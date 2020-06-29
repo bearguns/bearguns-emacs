be6e74707525ea654e5c340f45f84b366cd0bf93
Sean Brage <sean.brage@teliax.com>
2020-06-29 11:40:22 -0600

    feat: Cleans up default settings and backup file handling

--- init.el
+++ init.el
@@ -4,6 +4,7 @@
 ;;; Code:
 ;; Load files from our ~lisp~ directory:
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(require 'bg-defaults)
 
 (require 'bg-elpa)
 ;; Load our Elpa configuration for fetching packages.
@@ -18,19 +19,10 @@
 ;; DWIM and autocompletion
 
 (require 'bg-org)
+
+(require 'bg-markup)
+
+(require 'bg-lint)
+
+(require 'bg-git)
 ;;; init.el ends here.
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(evil-escape-mode t)
- '(global-display-line-numbers-mode t)
- '(package-selected-packages
-   '(markdown-mode yaml-mode editorconfig prettier-js flycheck rainbow-delimiters org-journal org-pomodoro company counsel ivy neotree evil-escape evil spaceline all-the-icons nyan-mode golden-ratio rebecca-theme)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
