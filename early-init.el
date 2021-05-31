f8e126137fd7372aa9860d4f1504a215c384b97e
Sean Brage <sean.brage@teliax.com>
2021-05-30 21:22:34 -0600

    feat: EVIL + General cleanup

--- early-init.el
+++ early-init.el
@@ -1,13 +1,3 @@
-;; configure Emacs garbage collector, which is somewhat famous for
-;; possibly slowing down your editor
-(setq gc-cons-threshold most-positive-fixnum)
-(setq gc-cons-percentage 0.6)
-
-;; Don't enable Emacs' package system yet!
-;; We will do that later at a more optimal time.
-(setq package-enable-at-startup nil)
-(setq package-quickstart nil)
-
 ;; Hide UI elements early, don't flash vanilla Emacs at me
 ;; and make me think something's broken.
 (push '(menu-bar-lines . 0) default-frame-alist)
@@ -23,6 +13,3 @@
 (scroll-bar-mode -1)
 (setq inhibit-splash-screen 1)
 (setq use-file-dialog nil)
-
-;; Minimize compilation of packages and core
-(setq comp-deferred-compilation nil)
